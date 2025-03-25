047f6ea25a2467d5a9c0f00a7a705967ab7e55f8
GonzaloSM <102286508+gonzashan@users.noreply.github.com>
2025-03-25 17:07:08 +0100

    Update main-script.js

--- PROJECT_TIMELINE/js/main-script.js
+++ PROJECT_TIMELINE/js/main-script.js
@@ -436,7 +436,7 @@ async function inicio2() {
         await cargarLiquidaciones(); // Espera a que cargarLiquidaciones termine
         await obtenerDatos();
 
-        fetch('https://raw.githubusercontent.com/gonzashan/M17/e8fb70a6cf3bfee92668d862eb9942728f3d806c/PROJECT_TIMELINE/timeline_data.json')
+        fetch('../json/timeline_data.json')
             .then(function (response) {
                 if (!response.ok) {
                     throw new Error('No se pudo cargar el archivo JSON.');
